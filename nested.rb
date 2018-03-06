1c5e6c5fe77a1a13e68d5952c1b325c3647081eb
Aspen James <aspenjames@tqca.org>
2018-03-06 00:12:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -106,9 +106,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+   alans_new_info = "GUI"
+	 programmer_hash[:alan_kay][:known_for] = alans_new_info
+   programmer_hash
 
 end
 
@@ -129,4 +129,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
 end
